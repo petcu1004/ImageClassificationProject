93469067db46e61efb8179aa07fdfd715c8f0449
petcu1004 <msw4585@gmail.com>
2022-01-21 23:04:35 +0900

    갤러리에서 사진 가져오기 성공

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:io';//이미지 피커에서 골라온 사진을 파일로 활용하기 위해서 사용
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
+
 void main() => runApp(MyApp());
 
-final ImagePicker _picker = ImagePicker();
+
+
 
 //_image==null? Text('No image') : Image.file(File(_image.path)),
 
@@ -13,6 +17,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final ImagePicker _picker = ImagePicker();
   late PickedFile _image;
   @override
   Widget build(BuildContext context) {
@@ -22,14 +27,14 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(primarySwatch: Colors.red), //앱의 기본적인 디자인 테마 지정
       home: Scaffold(
         appBar: AppBar(
-          title: Text('포켓몬 찾기'), //AppBar 제목
-          centerTitle: true, //title 가운데 정렬
-          backgroundColor: Colors.redAccent, //AppBar 색상
-          elevation: 0.0, //그림자
+          title: Text('포켓몬 찾기'), 
+          centerTitle: true, 
+          backgroundColor: Colors.redAccent, 
+          elevation: 0.0, 
           toolbarHeight: 70, //AppBar 높이
         ),
         body: Padding(
-          padding: EdgeInsets.all(40),
+          padding: EdgeInsets.all(40), //모든 화면에서 40만큼 띄움
           child: Column(
             children: [
               Text(
@@ -37,8 +42,31 @@ class _MyAppState extends State<MyApp> {
                 //텍스트 스타일에서 폰트 사이즈 조절
                 style: TextStyle(fontSize: 20, letterSpacing: 1.5),
               ),
+              SizedBox(height: 40,), 
+              _image==null?
+              Container(
+                decoration: BoxDecoration(
+                border: Border.all(
+                color:Colors.black,
+                style:BorderStyle.solid,
+                width: 2
+                )
+                ),
+                child: Center(child: Text("<No Image>"),),
+                width: 500,
+                height: 450,
+                
+              )
+              : Image.file(File(_image.path)),
+
+              
+              SizedBox(height: 40,), 
+
+              ElevatedButton(onPressed: (){}, child: Text('찾기'),
+              )
             ],
           ),
+          
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: _getImage,
@@ -52,6 +80,7 @@ class _MyAppState extends State<MyApp> {
     PickedFile image = await _picker.getImage(source: ImageSource.gallery);
     setState(() {
       _image = image;
+
     });
   }
 }
