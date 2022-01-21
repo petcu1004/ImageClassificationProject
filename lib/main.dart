faef771e602d8b52004711ffdbdd83597f1d73b1
petcu1004 <msw4585@gmail.com>
2022-01-22 03:19:34 +0900

    메인 화면 만들고, 첫번쨰 화면에서 두번쨰 화면으로 이동 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,33 @@
-import 'dart:io';//이미지 피커에서 골라온 사진을 파일로 활용하기 위해서 사용
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:flutter_application_first_toy_project/pokemonFind.dart';
+import 'package:flutter_application_first_toy_project/pokemonFound.dart';
+import 'package:flutter_application_first_toy_project/pokemonNotFound.dart';
+import 'package:flutter_application_first_toy_project/start.dart';
+
+// import 'package:ImageClassificationProject/pokemonFind.dart';
+// import 'package:ImageClassificationProject/pokemonFound.dart';
+// import 'package:ImageClassificationProject/pokemonNotFound.dart';
 
 
-void main() => runApp(MyApp());
-
-
-
-
-//_image==null? Text('No image') : Image.file(File(_image.path)),
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main(){
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  final ImagePicker _picker = ImagePicker();
-  late PickedFile _image;
+class MyApp extends StatelessWidget {
+
+  static const String _title = 'Pokemon';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, //오른쪽 위 디버그 라벨 삭제
-      title: '포켓몬',
-      theme: ThemeData(primarySwatch: Colors.red), //앱의 기본적인 디자인 테마 지정
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('포켓몬 찾기'), 
-          centerTitle: true, 
-          backgroundColor: Colors.redAccent, 
-          elevation: 0.0, 
-          toolbarHeight: 70, //AppBar 높이
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(40), //모든 화면에서 40만큼 띄움
-          child: Column(
-            children: [
-              Text(
-                '<찾고싶은 포켓몬 사진을 가져오세요>',
-                //텍스트 스타일에서 폰트 사이즈 조절
-                style: TextStyle(fontSize: 20, letterSpacing: 1.5),
-              ),
-              SizedBox(height: 40,), 
-              _image==null?
-              Container(
-                decoration: BoxDecoration(
-                border: Border.all(
-                color:Colors.black,
-                style:BorderStyle.solid,
-                width: 2
-                )
-                ),
-                child: Center(child: Text("<No Image>"),),
-                width: 500,
-                height: 450,
-                
-              )
-              : Image.file(File(_image.path)),
-
-              
-              SizedBox(height: 40,), 
-
-              ElevatedButton(onPressed: (){}, child: Text('찾기'),
-              )
-            ],
-          ),
-          
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _getImage,
-          child: Icon(Icons.add_a_photo),
-        ),
-      ),
+      title : _title,
+      initialRoute: '/',
+      routes:{
+        '/' : (context) => Start(),
+        '/first' : (context) => PokemonFind(),
+        '/second':(context) => PokemonFound(),
+        '/thied':(context) => PokemonNotFound(),
+      },
     );
-  }
-
-  Future _getImage() async {
-    PickedFile image = await _picker.getImage(source: ImageSource.gallery);
-    setState(() {
-      _image = image;
-
-    });
   }
 }
--- lib/pokemonFind.dart
+++ lib/pokemonFind.dart
@@ -1,37 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'dart:io';//이미지 피커에서 골라온 사진을 파일로 활용하기 위해서 사용
 
 class PokemonFind extends StatefulWidget {
-  const PokemonFind({ Key? key }) : super(key: key);
+  // const PokemonFind({ Key? key }) : super(key: key);
 
   @override
   _PokemonFindState createState() => _PokemonFindState();
 }
 
 class _PokemonFindState extends State<PokemonFind> {
-  //PickedFile f = await ImagePicker().getImage(source: ImageSource.gallery);
-  //File dummyFile = File(f.path);
-
+  final ImagePicker _picker = ImagePicker();
+  late PickedFile _image;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('포켓몬 찾기'),
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            //
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/found');
-                },
-                child: Text('찾고 싶은 포켓몬은'),
-              ),
-            )
-          ],
+        appBar: AppBar(
+          title: Text('포켓몬 찾기'), 
+          centerTitle: true, 
+          backgroundColor: Colors.redAccent, 
+          elevation: 0.0, 
+          toolbarHeight: 70, //AppBar 높이
         ),
-      ),
+        body: Padding(
+          padding: EdgeInsets.all(40), //모든 화면에서 40만큼 띄움
+          child: Column(
+            children: [
+              Text(
+                '<찾고싶은 포켓몬 사진을 가져오세요>',
+                //텍스트 스타일에서 폰트 사이즈 조절
+                style: TextStyle(fontSize: 20, letterSpacing: 1.5),
+              ),
+              SizedBox(height: 40,), 
+              _image==null?
+              Container(
+                decoration: BoxDecoration(
+                border: Border.all(
+                color:Colors.black,
+                style:BorderStyle.solid,
+                width: 2
+                )
+                ),
+                child: Center(child: Text("<No Image>"),),
+                width: 500,
+                height: 450,
+                
+              )
+              : Image.file(File(_image.path)),
+
+              
+              SizedBox(height: 40,), 
+
+              ElevatedButton(onPressed: (){
+                Navigator.of(context).pushNamed('/second');
+              }, child: Text('찾기'),
+              )
+            ],
+          ),
+          
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _getImage,
+          child: Icon(Icons.add_a_photo),
+        ),
+      
     );
   }
+
+  Future _getImage() async {
+    PickedFile image = await _picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      _image = image;
+
+    });
+  }
+
+
 }
